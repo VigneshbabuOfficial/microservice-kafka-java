4338e44765c5d7f4abc36d5a8ae53f4126eee5b0
VigneshbabuA <vigneshofficial2020@gmail.com>
2022-08-06 04:17:22 +0530

    initial commit

---
+++ kafka/kafka-consumer/src/main/java/com/kafkaconsumer/config/KafkaConsumerConfig.java
@@ -0,0 +1,59 @@
+package com.kafkaconsumer.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+	// config for String plain text
+
+		@Bean
+		public ConsumerFactory<String, String> consumerFactory() {
+			Map<String, Object> configs = new HashMap<>();
+			configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+			configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+			configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+			configs.put(ConsumerConfig.GROUP_ID_CONFIG, "topic-1");
+			return new DefaultKafkaConsumerFactory<>(configs);
+		}
+
+		@Bean
+		public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+			ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+			factory.setConsumerFactory(consumerFactory());
+			return factory;
+		}
+		
+		// config for json data - ObjectNode
+
+		@Bean
+		public ConsumerFactory<String, ObjectNode> objectNodeConsumerFactory() {
+			Map<String, Object> configs = new HashMap<>();
+			configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+			configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+			configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+			configs.put(ConsumerConfig.GROUP_ID_CONFIG, "topic-2");
+			return new DefaultKafkaConsumerFactory<>(configs, new StringDeserializer(), new JsonDeserializer<>(ObjectNode.class));
+		}
+
+		@Bean
+		public ConcurrentKafkaListenerContainerFactory<String, ObjectNode> objectNodeKafkaListenerContainerFactory() {
+			ConcurrentKafkaListenerContainerFactory<String, ObjectNode> factory = new ConcurrentKafkaListenerContainerFactory<String, ObjectNode>();
+			factory.setConsumerFactory(objectNodeConsumerFactory());
+			return factory;
+		}
+}
